56f7583ac4c580f4b68c080ef987d1a3a0df9443
rkaradas <richi2k@live.de>
2015-07-26 14:47:08 +0200

    index.php modified

--- index.php
+++ index.php
@@ -7,24 +7,30 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Javascript Unpacker</title>
+		<title>Javascript unpacker testpage</title>
 		<meta name="description" content="Unpacker for JavaScript files that were packed with Dean Edwards JavaScript's Packer.">
 		<meta name="author" content="Recep Karadas">
 	</head>
 	<body>
-		
+		<h1>Javascript unpacker testpage</h1>
 		<form method="POST">
-<?php
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$jsu = new JavaScriptUnpacker();
-		echo $jsu->Unpack($_POST["userInput"]);
-	}else{
-?>			
-			<textarea type="text" name="userInput" ></textarea>
-			<input type="submit" value="submit"/>
-<?php	
-	}
-?>
+			<div style="padding:10px;background:#efefef;display:inline-block;">
+			<?php
+				if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+					$jsu = new JavaScriptUnpacker();
+			?>
+				<h4>Result: </h4>
+			<?php
+					echo $jsu->Unpack($_POST["userInput"]);
+				}else{
+			?>			
+				<label for="userInput">Put in your packed javascript:</label><br />
+				<textarea type="text" name="userInput" rows="10" cols="50" ></textarea><br />
+				<input type="submit" value="submit" style="float:right"/>
+			<?php	
+				}
+			?>
+			</div>
 		</form>
 	</body>
 </html>
